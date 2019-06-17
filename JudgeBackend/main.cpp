fd919a94d84116fd816d0948168f89a5e35fe128
郑文鑫 <peterzheng@zhengwenxindeMacBook-Pro.local>
2019-06-17 15:04:02 +0800

    Reconstruct the project and start judging part

--- JudgeBackend/main.cpp
+++ JudgeBackend/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include "Utility.h"
-#include "init.h"
 void help() {
   printf(
       "Compiler Judge Backend %.1lf Build %d"
@@ -12,16 +11,12 @@ void help() {
       "\n\nHope you all get good scores!", VERSION, BUILD_VERSION);
   exit(0);
 }
-compilerBackend::init JudgeInit;
 
 
+/* The program should run with the following arguments:
+ * ./JudgeCore compile <compile bash name> <output file name>
+ * ./JudgeCore run <
+ */
 int main(int argc, char** argv) {
-  if (argc <= 2) help();
-  if (argc == 3 && std::string(argv[1]) == std::string("init")){ // init function here
-    std::string fileName = std::string(argv[2]);
-    JudgeInit.setFileName(fileName);
-    JudgeInit.run();
-  } else if(argc == 3 && std::string(argv[1]) == std::string("start")){
-    std::string configFile = std::string(argv[2]);
-  } else help();
+    char* runningMode = argv[1];
 }
--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Est. Finish Time: N/A
 
 ## Overview
 ![Overview](https://github.com/peterzheng98/CompilerJudge/blob/master/Overview.png)
+
+## Judge Part
+[Java]Judge Frontend: Use Java to activate docker for build and to run the compiling stage with outputing assembly code.
+[C++]Judge Core: Run the score of the assembly code.
